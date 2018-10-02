98d4ecd247a443041b33359832c93527770ade1d
Henning Kayser <henningkayser@mail.de>
2018-10-03 00:31:20 +0200

    Publish bottle pose and label markers

--- src/detect_bottles_action.cpp
+++ src/detect_bottles_action.cpp
@@ -8,9 +8,12 @@
 #include <tf/transform_listener.h>
 #include <tf_conversions/tf_eigen.h>
 
+#include <visualization_msgs/Marker.h>
+
 #include <geometric_shapes/mesh_operations.h>
 #include <geometric_shapes/shape_operations.h>
 #include <geometric_shapes/shape_extents.h>
+
 
 
 //std::string BOTTLE_MESH = "package://tams_bartender_recognition/meshes/bottle-binary.stl";
@@ -35,6 +38,7 @@ class BottleActionServer
     std::map<std::string, int> object_count_;
     ros::ServiceClient segmentation_client_;
 
+    ros::Publisher marker_pub_;
 
     bool recognize_objects_ = false;
 
@@ -48,24 +52,34 @@ class BottleActionServer
     }
 
 
-    bool createCollisionObject(std::string id, const tams_bartender_recognition::RecognizedObject& obj, moveit_msgs::CollisionObject& object) {
+    bool createCollisionObject(int i, std::string id, const tams_bartender_recognition::RecognizedObject& obj, moveit_msgs::CollisionObject& object) {
 
       // add mesh to object
       collisionObjectFromResource(object, id, BOTTLE_MESH);
-      double mesh_height = computeMeshHeight(object.meshes[0]);
+      object.header.frame_id = camera_frame_;
 
       // move object pose to half of mesh height
       geometry_msgs::Pose pose = obj.pose.pose;
+      double mesh_height = computeMeshHeight(object.meshes[0]);
       pose.position.z = 0.5 * mesh_height + 0.002;
+      pose.orientation.w = 1.0;
 
       // transform object pose to camera frame
       tf::Transform camera_surface_transform;
       tf::Transform surface_obj_transform;
       tf::poseMsgToTF(pose, surface_obj_transform);
       tf::transformMsgToTF(obj.surface_transform, camera_surface_transform);
-      object.mesh_poses.resize(1);
       tf::poseTFToMsg(camera_surface_transform * surface_obj_transform, object.mesh_poses[0]);
-      object.header.frame_id = camera_frame_;
+
+      // publish bottle marker
+      publishBottleMarker(i, id, camera_frame_, object.mesh_poses[0]);
+
+      // publish label above bottle
+      pose.position.z += mesh_height;
+      tf::Transform surface_label_transform;
+      tf::poseMsgToTF(pose, surface_label_transform);
+      tf::poseTFToMsg(camera_surface_transform * surface_label_transform, pose);
+      publishTextMarker(i, id, camera_frame_, pose);
 
       return true;
     }
@@ -94,6 +108,46 @@ class BottleActionServer
       geometric_shapes::getShapeExtents(mesh, x, y, z);
       return z;
     }
+
+    void publishBottleMarker(int id, const std::string& label, const std::string& frame_id, const geometry_msgs::Pose& pose)
+    {
+      visualization_msgs::Marker m;
+      m.header.frame_id = frame_id;
+      m.header.stamp = ros::Time::now();
+      m.type = visualization_msgs::Marker::CYLINDER;
+      m.pose = pose;
+      m.action = visualization_msgs::Marker::ADD;
+      m.id = id;
+      m.ns = "cylinders";
+      m.scale.x = 0.075;
+      m.scale.y = 0.075;
+      m.scale.z = 0.25;
+      m.color.g = 1.0;
+      m.color.a = 0.5;
+      m.lifetime = ros::Duration(10.0);
+      marker_pub_.publish(m);
+    }
+
+    void publishTextMarker(int id, const std::string& label, const std::string& frame_id, const geometry_msgs::Pose& pose)
+    {
+      visualization_msgs::Marker t;
+      t.header.frame_id = frame_id;
+      t.header.stamp = ros::Time::now();
+      t.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+      t.action = visualization_msgs::Marker::ADD;
+      t.text = label;
+      t.pose = pose;
+      t.id = id;
+      t.ns = "labels";
+      t.scale.z = 0.03;
+      t.color.r = 1.0;
+      t.color.g = 1.0;
+      t.color.b = 1.0;
+      t.color.a = 1.0;
+      t.lifetime = ros::Duration(30.0);
+      marker_pub_.publish(t);
+    }
+
 
   protected:
 
@@ -132,13 +186,14 @@ class BottleActionServer
         return;
       }
 
+      int i = 0;
       tiago_bartender_msgs::DetectBottlesResult result;
       std::vector<moveit_msgs::CollisionObject> objs;
       for (std::map<std::string,int>::iterator it=object_count_.begin(); it!=object_count_.end(); ++it) {
         std::string id = it->first;
         if(object_count_[id] >= goal->stability_threshold) {
           moveit_msgs::CollisionObject object;
-          if(createCollisionObject(id, objects_[id], object)) {
+          if(createCollisionObject(i++, id, objects_[id], object)) {
             objs.push_back(object);
           }
           result.detected_bottles.push_back(id);
@@ -160,6 +215,8 @@ class BottleActionServer
 
     segmentation_client_ = nh_.serviceClient<tams_bartender_recognition::SegmentationSwitch>("object_segmentation_switch");
     object_pose_sub = nh_.subscribe("object_poses", 1, &BottleActionServer::object_pose_cb, this);
+    marker_pub_ = nh_.advertise<visualization_msgs::Marker> ("/detected_bottles", 1);
+
     as_.start();
   }
 
